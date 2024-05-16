f580f0adcf5222f36574237d93d0a969d5a95e30
LorenzaBriz0t0 <lbrizoto22@usp.br>
2024-05-16 13:06:29 -0300

    commit-condicionais-compostas

--- ProjetoJava/src/Anotações.java
+++ ProjetoJava/src/Anotações.java
@@ -1,2 +1,9 @@
 // Condicional Simples
 // Exemplo em "CaixaEletronico.java
+ 
+// Condicionais Compostas
+// If e Else
+// Ver "ResultadoEscolar.java" e "CaixaEletronico.java" para exemplos
+// Particularidades -> Condição entre "()"
+// Se o if tiver mais de uma instrução, ele deve ser separado por um bloco de "{}"
+
--- ProjetoJava/src/CaixaEletronico.java
+++ ProjetoJava/src/CaixaEletronico.java
@@ -2,12 +2,15 @@ public class CaixaEletronico {
     public static void main(String[] args){
 
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
-        if (valorSolicitado > saldo);
-        saldo = saldo -valorSolicitado;
+        if (valorSolicitado < saldo){
+            saldo = saldo -valorSolicitado;
 
-        System.out.println("Seu saldo atual é:\nR$" + saldo + "\n Valor do último saque:\nR$" + valorSolicitado);
+            System.out.println("Seu saldo atual é:\nR$" + saldo + "\n Valor do último saque:\nR$" + valorSolicitado);
+        }else
+            System.out.println("Saldo insuficiente");
+
 
 
     }
