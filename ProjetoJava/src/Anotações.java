76d126f9e62bdbae2a7ad847edd2d93fd357d297
LorenzaBriz0t0 <lbrizoto22@usp.br>
2024-07-22 22:57:30 -0300

    For

--- ProjetoJava/src/Anotações.java
+++ ProjetoJava/src/Anotações.java
@@ -24,3 +24,15 @@
 // Switch Case
 // Comparação de medidas
 // Exemplo em "SistemaMedida.java"
+
+// For
+// Contadores e testes
+// for (bloco de inicialização; expressão booleana de validação; bloco de atualização ou incrementação)
+//{ 
+    //comando que será executado até que a expressão de validação se torne falsa
+//}
+
+
+
+
+
